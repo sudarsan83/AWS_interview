ac6da4cc9a8cea628c98e30058baeab72ddf0430
sudarsan83 <gouravamsudha@gmail.com>
2026-01-14 15:36:50 +0530

    update devops

--- Docker/python-web-app/demo/views.py
+++ Docker/python-web-app/demo/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to DevOps Zero to Hero by Abhishek Veeramalla")
+    return HttpResponse("Welcome to Sudarsan Gouravam WEBPAGE")
